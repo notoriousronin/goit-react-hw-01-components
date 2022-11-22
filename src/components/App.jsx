4158c538e8a39e4cfc32ae9adc50c85e639c6604
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-11-22 17:55:06 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
+import Profile from './Profile/profile';
+import user from 'dataJSON/user.json';
+import data from 'dataJSON/data.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
-};
+}
