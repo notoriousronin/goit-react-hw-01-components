6b77d7094235286964b8f03bcba57fbc54803e45
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-11-27 19:16:31 +0200

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import Profile from './Profile/Profile';
-import Statistics from './Statistics/Statistics';
+import Profile from './Profile/profile';
+import Statistics from './Statistics/statistics';
 import user from 'dataJSON/user.json';
 import data from 'dataJSON/data.json';
 import GlobalStyles from './GlobalStyles';
